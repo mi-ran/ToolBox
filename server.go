9fc6a572b85c42da245cdd93178fc1dcfd919790
mi-ran <azsxdc8911@gmail.com>
2019-05-23 23:43:13 +0900

    [190523] 리그레션 url 추가

--- server.go
+++ server.go
@@ -12,7 +12,7 @@ var count int
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/count", counter)
+	http.HandleFunc("/regression", regression_tester)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
@@ -23,8 +23,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
-	mu.Unlock()
+func regression_tester(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
